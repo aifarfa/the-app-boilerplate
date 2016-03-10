992690392bbf15ef1462077e57ffbbe0bcfda909
pasit.r <pasit.rattanathammak@thomsonreuters.com>
2016-03-10 18:47:15 +0700

    update sample test

--- test/server/sample-test.js
+++ test/server/sample-test.js
@@ -6,25 +6,33 @@ const sample = require('../../services/sample');
 
 describe('sample', () => {
 
-  let resp;
-
   beforeEach(() => {
-    resp = {
-      send: sinon.spy()
+    // mockup Express.Reponse
+    this.response = {
+      send: sinon.spy(),
+      sendStatus: sinon.spy()
     };
   });
 
-  it('defined a function', () => {
+  it('defined who function', () => {
     expect(sample.who).to.be.a('function');
   });
 
   it('should call resp.send with status 200', () => {
+    const res = this.response;
     const context = {
       'user': {
         'uuid': 'aaa'
       }
     };
-    sample.who(context, null, null, resp);
-    expect(resp.send.calledWith(200)).to.be.ok;
+
+    sample.who(context, null, null, res);
+    // console.log(resp.send.firstCall.args);
+
+    expect(res.sendStatus.called).to.be.false;
+    expect(res.send.firstCall.args[0]).to.eq(200);
+    expect(res.send.firstCall.args[1]).to.eql({
+      'uuid': 'aaa'
+    });
   });
 });
